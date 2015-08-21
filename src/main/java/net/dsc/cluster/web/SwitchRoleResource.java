6dc5cd5a8721a0cafa1defd40826c40eb4f21d2a
magicwolf <dai.dongliang@foxmail.com>
2015-08-21 18:48:31 +0800

    Signed-off-by: magicwolf <dai.dongliang@foxmail.com>

--- src/main/java/net/dsc/cluster/web/SwitchRoleResource.java
+++ src/main/java/net/dsc/cluster/web/SwitchRoleResource.java
@@ -14,7 +14,7 @@
  *    under the License.
  **/
 
-package net.floodlightcontroller.core.web;
+package net.dsc.cluster.web;
 
 import java.io.IOException;
 import java.lang.Thread.State;
@@ -46,6 +46,7 @@ import net.dsc.hazelcast.message.RoleMessage;
 import net.floodlightcontroller.core.IFloodlightProviderService;
 import net.floodlightcontroller.core.IOFSwitch;
 import net.floodlightcontroller.core.internal.IOFSwitchService;
+import net.floodlightcontroller.core.web.CoreWebRoutable;
 
 import org.restlet.resource.Get;
 import org.slf4j.Logger;
